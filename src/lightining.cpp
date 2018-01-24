48f423f4beb87f1e19f39c5493c5271c366a208d
alepmaros <alehstk@gmail.com>
2018-01-23 23:35:09 -0200

    Better Lighting

--- src/lightining.cpp
+++ src/lightining.cpp
@@ -197,13 +197,17 @@ int main()
         lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
-        lightingShader.setVec3("lightPos", lightPos);
+        
 
-        // Material
+        // Material & Light
         lightingShader.setVec3("material.ambient",  1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         lightingShader.setFloat("material.shininess", 32.0f);
+        lightingShader.setVec3("lightPos", lightPos);
+        lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // darken the light a bit to fit the scene
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f); 
                 
         model = glm::mat4(1.0f);
         //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
